f8aeaea805ae1d492369b9bc3235a2aa1ca48338
moniwaterhouse <101612925+moniwaterhouse@users.noreply.github.com>
2022-10-29 12:31:06 -0600

    [Update] System integration with the memory controller

--- dmem_rom.sv
+++ dmem_rom.sv
@@ -1,13 +1,18 @@
-module dmem_rom (input logic [31:0] address,
-				output logic [31:0] rd);
-	
-	logic [31:0] dmem_ROM[0:149999];
-	
-	initial
-
-		//$readmemh("C:/MySpot/ComputerArchitecture2.Project2/TextFiles/imageData.txt", dmem_ROM);
-	   $readmemh("C:/TextFiles/imageData.txt", dmem_ROM);	
-		
-	assign rd = dmem_ROM[address[31:0]];
-	
-endmodule 
+module dmem_rom
+#(
+    parameter V=192,
+    parameter SIZE=150000
+)
+(
+    input  logic        VecOp,
+    input  logic[V-1:0] address,
+    output logic[V-1:0] rd
+);
+    logic [V-1:0] dmem_ROM[0:SIZE-1];
+    
+    initial begin
+       $readmemh("C:/MySpot/ComputerArchitecture2.Project2/TextFiles/imageData.txt", dmem_ROM);
+    end
+        
+    assign rd = dmem_ROM[address[V-1:0]];
+endmodule : dmem_rom
