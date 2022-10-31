39ccbf9a9c9c7ca90d24a2b7898cec8dc080947c
Juan Igancio Navarro <ju.navarro@estudiantec.cr>
2022-10-31 08:36:23 -0600

    [ADD] - imem module and testbench

---
+++ imem.sv
@@ -0,0 +1,18 @@
+module imem
+#(
+	parameter N=32,
+	parameter INS = 1000
+)
+(
+	input logic[N-1:0] pc,
+	output logic[N-1:0] instruction
+);
+
+	logic [N-1:0] imem_ROM[INS-1:0];
+	
+	initial
+       $readmemb("C:/Users/juan navarro/Documents/Implementaciones Arqui 2/ComputerArchitecture2.Project2/instructions.txt", imem_ROM);
+	
+	assign instruction = imem_ROM[pc[N-1:0]];
+	
+endmodule
---
+++ memoryController.sv
@@ -0,0 +1,12 @@
+module memoryController
+#(
+	parameter S = 32,
+	parameter V = 192,
+	parameter SIZE_INS = 1000,
+	parameter SIZE_ROM = 30000,
+	parameter SIZE_RAM = 30000
+)
+(
+);
+
+endmodule
